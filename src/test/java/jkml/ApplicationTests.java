4e3941c08805cc3193cc581a2c7a5c5bb5b875b0
jkmcl <jkmcl>
2022-02-06 01:30:59 +0800

    Clean up tests

--- src/test/java/jkml/ApplicationTests.java
+++ src/test/java/jkml/ApplicationTests.java
@@ -1,13 +1,21 @@
 package jkml;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.env.Environment;
 
 @SpringBootTest
 class ApplicationTests {
 
+	@Autowired
+	private Environment env;
+
 	@Test
 	void contextLoads() {
+		assertNotNull(env);
 	}
 
 }
--- src/test/java/jkml/web/TsvBuilderTests.java
+++ src/test/java/jkml/web/TsvBuilderTests.java
@@ -1,8 +1,7 @@
 package jkml.web;
 
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 import java.util.List;
 
 import org.junit.jupiter.api.Test;
@@ -11,18 +10,15 @@ import org.slf4j.LoggerFactory;
 
 class TsvBuilderTests {
 
-	private static final Path OUT_DIR = Path.of("target/test-classes");
-
 	private final Logger log = LoggerFactory.getLogger(getClass());
 
 	@Test
-	void test() throws IOException {
-		Path filePath = OUT_DIR.resolve("tmp.tsv");
-
+	void test() {
 		TsvBuilder tb = new TsvBuilder(List.of("fileType", "baseName", "start", "stop"));
 		tb.addRecord(List.of("master", "1234", "00:00:00", "23:59:59"));
-		log.info(tb.toString());
-		Files.writeString(filePath, tb.toString());
+		String tsv = tb.toString();
+		assertNotNull(tsv);
+		log.info(tsv);
 	}
 
 }
